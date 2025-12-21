cc6f42f6bd4caf1f3c4e1e350b770480ff7f9741
Maheshdeekonda428 <maheshdeekonda45@gmail.com>
2025-12-21 15:13:33 +0530

    calc.py added

---
+++ mcp/calc_mcp/calc.py
@@ -0,0 +1,50 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(name="cal_mcp")
+
+@mcp.tool()
+def add(a:int|float, b:int|float) -> int|float:
+    """Adds two numbers
+    Args:
+        a(int): number
+        b(int): number
+    Returns:
+        int: a + b
+    """
+    return a + b
+
+@mcp.tool()
+def subtract(a:int|float, b:int|float) -> int|float:
+    """subtracts two numbers
+    Args:
+        a(int): number
+        b(int): number
+    Returns:
+        int: a - b
+    """
+    return a - b
+
+@mcp.tool()
+def multipy(a:int|float, b:int|float) -> int|float:
+    """multipies two numbers
+    Args:
+        a(int): number
+        b(int): number
+    Returns:
+        int: a * b
+    """
+    return a * b
+
+@mcp.tool()
+def divide(a:int|float, b:int|float) -> int|float:
+    """divides two numbers
+    Args:
+        a(int): number
+        b(int): number
+    Returns:
+        int: a / b
+    """
+    return a / b
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
