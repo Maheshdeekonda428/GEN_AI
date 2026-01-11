e4d80b75863f3f7a6f35130bec6d358417e4813b
Maheshdeekonda428 <maheshdeekonda45@gmail.com>
2026-01-11 18:21:33 +0530

    Added changes to include docker file

---
+++ mcp/fastmcp_http/server.py
@@ -0,0 +1,19 @@
+from fastmcp import FastMCP
+from mcp.server.fastmcp import FastMCP
+
+
+mcp = FastMCP("Streamable http")
+
+@mcp.tool()
+def add(a: int, b: int):
+    """Adds two numbers"""
+    return a + b
+
+@mcp.tool()
+def echo(message: str):
+    """Echoes a string"""
+    return f"you said {message}"
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="0.0.0.0", port=8000)
+
